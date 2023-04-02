486f7a4516c09dadeca8bb5d0002fcaf18733b66
Maksym Solomko <max.exc7@gmail.com>
2023-04-02 22:48:29 +0200

    header & gallery

---
+++ components/Categories.js
@@ -0,0 +1,24 @@
+import { Text, ScrollView } from 'react-native';
+import React from 'react'
+import CategoryCard from './CategoryCard';
+
+const Categories = () => {
+  return (
+    <ScrollView
+      contentContainerStyle={{
+        paddingHorizontal: 15,
+        paddingTop: 10,
+      }}
+      horizontal
+      showsHorizontalScrollIndicator={false}
+    >
+
+      {/* CategoryCard */}
+      <CategoryCard imgUrl="https://links.papareact.com/wru" title="Testing 1"/>
+      <CategoryCard imgUrl="https://links.papareact.com/wru" title="Testing 2" />
+      <CategoryCard imgUrl="https://links.papareact.com/wru" title="Testing 3"/>
+    </ScrollView>
+  )
+}
+
+export default Categories;
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,11 +1,72 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, SafeAreaView, Image, TextInput, ScrollView } from 'react-native';
+import React, { useLayoutEffect } from 'react';
+import { useNavigation } from '@react-navigation/native';
+import {  
+    AdjustmentsVerticalIcon,
+    ChevronDownIcon,
+    MagnifyingGlassIcon,
+    UserIcon,
+} from "react-native-heroicons/outline";
+import Categories from "../components/Categories";
 
 const HomeScreen = () => {
+    const navigation = useNavigation();
+
+    useLayoutEffect(() => {
+        navigation.setOptions({
+            // headerTitle: "TOAST",
+            headerShown: false,
+        })
+    }, [])
+
   return (
-    <View>
-      <Text className="text-red-500">HomeScreen</Text>
-    </View>
+    <SafeAreaView className="bg-white pt-8 " >
+        {/* Header */}
+        <View className="flex-row pb-3 items-center mx-4 space-x-2">
+            <Image
+                source={{
+                    uri: "https://payload.cargocollective.com/1/15/494563/13468564/roo-03_1340_c.jpg",
+                }}
+                className="h-7 w-7 bg-gray-300 p-6 rounded-full"
+            />          
+            <View className="flex-1">
+                  <Text className="font-bold text-gray-400 text-xs">
+                      Deliver Now!
+                  </Text>
+                  <Text className="font-bold text-xl">
+                      Current Location
+                      <ChevronDownIcon size={20} color="#00CCBB" />
+                  </Text>
+            </View>
+            <UserIcon size={35} color="#00CCBB" />
+        </View>
+        
+        {/* Search */}
+        <View className="flex-row items-center space-x-2 pb-2 mx-4">
+            <View className="flex-row flex-1 space-x-2 bg-gray-200 p-3 rounded-lg">
+                  <MagnifyingGlassIcon color="#00CCBB" size={20} />
+                <TextInput                    
+                    placeholder='Restaurants and cuisines'
+                    keyboardType='default'                  
+                />                
+            </View>
+            
+            <AdjustmentsVerticalIcon color="#00CCBB" />
+        </View>
+        
+        {/* Body */}
+        <ScrollView 
+            className='bg-gray-100 flex-1'
+            contentContainerStyle={{
+                paddingBottom: 100,
+            }}
+        >
+            {/* Categories */}
+            <Categories />
+            {/* Featured Rows */}
+        </ScrollView>
+      {/* <Text className="text-red-500">HomeScreen</Text> */}
+    </SafeAreaView>
   )
 }
 
